9e79ea565b908e0cb2742da9d7ae4cc99d3a80ff
Mike Solomon <mike@meeshkan.com>
2019-09-22 15:06:40 +0300

    Adds new test files

---
+++ test/1-hello-unmock/2-same-with-an-api.test.ts
@@ -0,0 +1,45 @@
+// now, let's look at a function that fetches users from the API
+// and performs the same operation.
+
+// our real API does not exist yet, so we use unmock.
+// but we have a problem - we forgot to turn unmock on! read how
+// on https://www.unmock.io/docs/introduction
+import unmock from "unmock";
+import axios from "axios";
+
+unmock
+  .nock("https://api.myservice.io")
+  .get("/users")
+  .reply(200, [
+    { id: 1, isAdmin: true },
+    { id: 2, isAdmin: false }
+  ]);
+
+interface User {
+  id: number;
+  isAdmin: boolean;
+}
+
+beforeAll(() => {
+  // uh oh, we forgot to turn unmock on!
+  // let's check https://www.unmock.io/docs/introduction
+  // and remember, *always* turn unmock off in the afterAll bloc
+  // (we've done it for you below)
+});
+afterAll(() => {
+  unmock.off()
+});
+
+const splitUsers = async () => {
+  const { data } = await axios("https://api.myservice.io/users");
+  return {
+    admin: data.filter((user: User) => user.isAdmin) as User[],
+    notAdmin: data.filter((user: User) => !user.isAdmin) as User[]
+  };
+}
+
+test("users from our API are split into admins and nonAdmins", async () => {
+  const split = await splitUsers();
+  expect(split.admin).toEqual([{ id: 1, isAdmin: true }]);
+  expect(split.notAdmin).toEqual([{ id: 2, isAdmin: false }]);
+});
---
+++ test/1-hello-unmock/3-use-variable-input.test.ts
@@ -0,0 +1,44 @@
+// let's use the same function, but with indeterminate results
+// we accomplish this using the `u` syntax in the reply method
+// below.
+// also, let's use the runner to test out different outcomes.
+// this is called fuzz testing: under the hood, the runner will
+// run your test 20 times
+
+// but wait, something does not seem quite right in the reply bloc...
+// we are generating an `id` field, but where did our `isAdmin`
+// field go? can you see how to generate it?
+
+import unmock, { u, runner } from "unmock";
+import axios from "axios";
+
+unmock
+  .nock("https://api.myservice.io")
+  .get("/users")
+  .reply(200, u.array({ id: u.number() /* we need an isAdmin field */ }));
+
+interface User {
+  id: number;
+  isAdmin: boolean;
+}
+
+beforeAll(() => unmock.on());
+afterAll(() => unmock.off());
+
+const splitUsers = async () => {
+  const { data } = await axios("https://api.myservice.io/users");
+  return {
+    admin: data.filter((user: User) => user.isAdmin) as User[],
+    notAdmin: data.filter((user: User) => !user.isAdmin) as User[]
+  };
+}
+
+test("randomly generated users from our API are split into admins and nonAdmins", runner(async () => {
+  const split = await splitUsers();
+  split.admin.forEach(user => {
+    expect(user.isAdmin).toBe(true);
+  })
+  split.notAdmin.forEach(user => {
+    expect(user.isAdmin).toBe(false);
+  })
+}));
