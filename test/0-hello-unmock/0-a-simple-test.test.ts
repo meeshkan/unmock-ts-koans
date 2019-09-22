f749fb2fd20b103137202de51272696188123736
Mike Solomon <mike@meeshkan.com>
2019-09-22 14:36:49 +0300

    Adds first test

---
+++ test/0-hello-unmock/0-a-simple-test.test.ts
@@ -0,0 +1,24 @@
+// let's look at a simple spec for a function
+// that splits arrays of users into an object containing
+// admins and nonAdmins
+
+// the test is failing. can you make it pass?
+
+interface User {
+  id: number;
+  isAdmin: boolean;
+}
+
+const splitUsers = (users: User[]) => ({
+  admin: users.filter(user => user.isAdmin),
+  notAdmin: users.filter(user => user.isAdmin)
+});
+
+test("users are split into admins and nonAdmins", () => {
+  const split = splitUsers([
+    { id: 1, isAdmin: true },
+    { id: 2, isAdmin: false }
+  ]);
+  expect(split.admin).toEqual([{ id: 1, isAdmin: true }]);
+  expect(split.notAdmin).toEqual([{ id: 2, isAdmin: false }]);
+});
