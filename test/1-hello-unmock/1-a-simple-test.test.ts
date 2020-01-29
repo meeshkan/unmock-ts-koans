b8ecf43e45541634e79edbdee0bd0c960d900292
Mike Solomon <mike@meeshkan.com>
2020-01-29 18:44:10 +0200

    Adds optional age field

--- test/1-hello-unmock/1-a-simple-test.test.ts
+++ test/1-hello-unmock/1-a-simple-test.test.ts
@@ -6,17 +6,18 @@
 
 interface User {
   id: number;
+  age?: number;
   isAdmin: boolean;
 }
 
 const splitUsers = (users: User[]) => ({
   admin: users.filter(user => user.isAdmin),
-  notAdmin: users.filter(user => user.isAdmin)
+  notAdmin: users.filter(user => !user.isAdmin)
 });
 
 test("users are split into admins and nonAdmins", () => {
   const split = splitUsers([
-    { id: 1, isAdmin: true },
+    { id: 1, age: 42, isAdmin: true },
     { id: 2, isAdmin: false }
   ]);
   expect(split.admin).toEqual([{ id: 1, isAdmin: true }]);
