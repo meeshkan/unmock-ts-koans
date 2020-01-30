a269fd97f6f527126924dfc5b7e46df818b15287
Mike Solomon <mike@meeshkan.com>
2020-01-30 11:51:41 +0200

    Removes spurious runner

--- test/1-hello-unmock/3-use-variable-input.test.ts
+++ test/1-hello-unmock/3-use-variable-input.test.ts
@@ -1,21 +1,22 @@
 // let's use the same function, but with indeterminate results
 // we accomplish this using the `u` syntax in the reply method
 // below.
-// also, let's use the runner to test out different outcomes.
-// this is called fuzz testing: under the hood, the runner will
-// run your test 20 times
 
 // but wait, something does not seem quite right in the reply bloc...
 // we are generating an `id` field, but where did our `isAdmin`
 // field go? can you see how to generate it?
 
-import unmock, { u, runner } from "unmock";
+import unmock, { u } from "unmock";
 import axios from "axios";
 
 unmock
   .nock("https://api.myservice.io")
   .get("/users")
-  .reply(200, u.array({ id: u.number() /* we need an isAdmin field */ }));
+  .reply(200, u.array({
+    id: u.number(),
+    age: u.opt(u.integer())
+    /* we need an isAdmin field */
+  }));
 
 interface User {
   id: number;
@@ -25,7 +26,7 @@ interface User {
 beforeAll(() => unmock.on());
 afterAll(() => unmock.off());
 
-const splitUsers = async () => {
+const fetchAndSplitUsers = async () => {
   const { data } = await axios("https://api.myservice.io/users");
   return {
     admin: data.filter((user: User) => user.isAdmin) as User[],
@@ -35,13 +36,13 @@ const splitUsers = async () => {
 
 test(
   "randomly generated users from our API are split into admins and nonAdmins",
-  runner(async () => {
-    const split = await splitUsers();
+  async () => {
+    const split = await fetchAndSplitUsers();
     split.admin.forEach(user => {
       expect(user.isAdmin).toBe(true);
     });
     split.notAdmin.forEach(user => {
       expect(user.isAdmin).toBe(false);
     });
-  })
+  }
 );
